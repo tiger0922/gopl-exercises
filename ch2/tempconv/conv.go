9bf2b773bbf00a6930b22078bae32d46286112a1
Anderson Tsai <tiger0922@gmail.com>
2020-01-08 15:51:33 +0800

    Finished Exercise 2.1, testing main folder named kelvin

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -13,4 +13,7 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
+func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
+
+func KtoC(k Kelvin) Celsius { return Celsius(k - 273.15) }
 //!-
