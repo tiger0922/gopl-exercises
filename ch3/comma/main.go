11bbf00af702f8409cd63521e66a7c242e8a9ce6
Anderson Tsai <tiger0922@gmail.com>
2020-01-13 18:16:16 +0800

    Finished Exercises 3.10 and 3.11

--- ch3/comma/main.go
+++ ch3/comma/main.go
@@ -17,24 +17,38 @@
 package main
 
 import (
-	"fmt"
-	"os"
+    "fmt"
+    "os"
 )
 
 func main() {
-	for i := 1; i < len(os.Args); i++ {
-		fmt.Printf("  %s\n", comma(os.Args[i]))
-	}
+    for i := 1; i < len(os.Args); i++ {
+        fmt.Printf("  %s\n", comma(os.Args[i]))
+    }
 }
 
 //!+
 // comma inserts commas in a non-negative decimal integer string.
 func comma(s string) string {
-	n := len(s)
-	if n <= 3 {
-		return s
-	}
-	return comma(s[:n-3]) + "," + s[n-3:]
+    n := len(s)
+    for i:= n-1; i > 0; i-- {
+        if s[i] == '.' {
+            n = i
+            break
+        }
+    }
+    /* Recursive Method
+    if n <= 3 {
+        return s
+    }
+    return comma(s[:n-3]) + "," + s[n-3:]
+    */
+    for i := n; i > 0; i-=3 {
+        if i-3 > 0 {
+            s = s[:i-3] + "," + s[i-3:]
+        }
+    }
+    return s
 }
 
 //!-
