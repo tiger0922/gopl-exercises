b50eac114afe0efea1cd4435d5ae6eea82a7e967
Anderson Tsai <tiger0922@gmail.com>
2020-01-30 17:45:47 +0800

    Finished Ex4.10

--- ch4/issues/main.go
+++ ch4/issues/main.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"gopl-exercises/ch4/github"
 )
@@ -21,10 +22,45 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%d issues:\n", result.TotalCount)
+	format := "#%-5d %9.9s %.55s\n"
+	now := time.Now()
+
+	pastDay := make([]*github.Issue, 0)
+	pastMonth := make([]*github.Issue, 0)
+	pastYear := make([]*github.Issue, 0)
+
+	day := now.AddDate(0, 0, -1)
+	month := now.AddDate(0, -1, 0)
+	year := now.AddDate(-1, 0, 0)
+
 	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n",
-			item.Number, item.User.Login, item.Title)
+		switch {
+		case item.CreatedAt.After(day):
+			pastDay = append(pastDay, item)
+		case item.CreatedAt.After(month) && item.CreatedAt.Before(day):
+			pastMonth = append(pastMonth, item)
+		case item.CreatedAt.After(year) && item.CreatedAt.Before(month):
+			pastYear = append(pastYear, item)
+		}
+	}
+
+	if len(pastDay) > 0 {
+		fmt.Printf("\nPast day:\n")
+		for _, item := range pastDay {
+			fmt.Printf(format, item.Number, item.User.Login, item.Title)
+		}
+	}
+	if len(pastMonth) > 0 {
+		fmt.Printf("\nPast month:\n")
+		for _, item := range pastMonth {
+			fmt.Printf(format, item.Number, item.User.Login, item.Title)
+		}
+	}
+	if len(pastYear) > 0 {
+		fmt.Printf("\nPast year:\n")
+		for _, item := range pastYear {
+			fmt.Printf(format, item.Number, item.User.Login, item.Title)
+		}
 	}
 }
 
