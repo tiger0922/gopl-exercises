3c9f785299c50053ea18c0b658054e593996c041
Anderson Tsai <tiger0922@gmail.com>
2020-06-10 16:56:54 +0800

    Ex6.1 ~ Ex.6.5 done

--- ch6/intset/intset.go
+++ ch6/intset/intset.go
@@ -7,67 +7,150 @@
 package intset
 
 import (
-	"bytes"
-	"fmt"
+    "bytes"
+    "fmt"
 )
+
+const wordSize = 32 << (^uint(0) >> 63)
 
 //!+intset
 
 // An IntSet is a set of small non-negative integers.
 // Its zero value represents the empty set.
 type IntSet struct {
-	words []uint64
+    words []uint
 }
 
 // Has reports whether the set contains the non-negative value x.
 func (s *IntSet) Has(x int) bool {
-	word, bit := x/64, uint(x%64)
-	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+    word, bit := x/wordSize, uint(x%wordSize)
+    return word < len(s.words) && s.words[word]&(1<<bit) != 0
 }
 
 // Add adds the non-negative value x to the set.
 func (s *IntSet) Add(x int) {
-	word, bit := x/64, uint(x%64)
-	for word >= len(s.words) {
-		s.words = append(s.words, 0)
-	}
-	s.words[word] |= 1 << bit
+    word, bit := x/wordSize, uint(x%wordSize)
+    for word >= len(s.words) {
+        s.words = append(s.words, 0)
+    }
+    s.words[word] |= 1 << bit
 }
 
 // UnionWith sets s to the union of s and t.
 func (s *IntSet) UnionWith(t *IntSet) {
-	for i, tword := range t.words {
-		if i < len(s.words) {
-			s.words[i] |= tword
-		} else {
-			s.words = append(s.words, tword)
-		}
-	}
+    for i, tword := range t.words {
+        if i < len(s.words) {
+            s.words[i] |= tword
+        } else {
+            s.words = append(s.words, tword)
+        }
+    }
+}
+
+func (s *IntSet) IntersectWith(t *IntSet) {
+    for i, tword := range t.words {
+        if i < len(s.words) {
+            s.words[i] &= tword
+        }
+    }
+}
+
+func (s *IntSet) DifferenceWith(t *IntSet) {
+    for i, tword := range t.words {
+        if i < len(s.words) {
+            s.words[i] &^= tword
+        }
+    }
+}
+
+func (s *IntSet) SymmetricDifferenceWith(t *IntSet) {
+    tmp := s.Copy()
+    s.UnionWith(t)
+    tmp.IntersectWith(t)
+    s.DifferenceWith(tmp)
 }
 
 //!-intset
+
+func popcount(x uint) int {
+    count := 0
+    for x != 0 {
+        count++
+        x &= x - 1
+    }
+    return count
+}
+
+// Return the number of elements
+func (s * IntSet) Len() int {
+    count := 0
+    for _, word := range s.words {
+        count += popcount(word)
+    }
+    return count
+}
+
+// Remove x from the set
+func (s *IntSet) Remove(x int) {
+    word, bit := x/wordSize, uint(x%wordSize)
+    s.words[word] &^= 1 << bit
+}
+
+// Remove all elements from the set 
+func (s * IntSet) Clear() {
+    for i := range s.words {
+        s.words[i] = 0
+    }
+}
+
+// Return a copy of the set 
+func (s *IntSet) Copy() *IntSet {
+    tmp := &IntSet{}
+    tmp.words = make([]uint, len(s.words))
+    copy(tmp.words, s.words)
+    return tmp
+}
+
+// Add a list of values
+func (s *IntSet) AddAll(nums ...int) {
+    for _, x := range nums {
+        s.Add(x)
+    }
+}
 
 //!+string
 
 // String returns the set as a string of the form "{1 2 3}".
 func (s *IntSet) String() string {
-	var buf bytes.Buffer
-	buf.WriteByte('{')
-	for i, word := range s.words {
-		if word == 0 {
-			continue
-		}
-		for j := 0; j < 64; j++ {
-			if word&(1<<uint(j)) != 0 {
-				if buf.Len() > len("{") {
-					buf.WriteByte(' ')
-				}
-				fmt.Fprintf(&buf, "%d", 64*i+j)
-			}
-		}
-	}
-	buf.WriteByte('}')
-	return buf.String()
+    var buf bytes.Buffer
+    buf.WriteByte('{')
+    for i, word := range s.words {
+        if word == 0 {
+            continue
+        }
+        for j := 0; j < wordSize; j++ {
+            if word&(1<<uint(j)) != 0 {
+                if buf.Len() > len("{") {
+                    buf.WriteByte(' ')
+                }
+                fmt.Fprintf(&buf, "%d", wordSize*i+j)
+            }
+        }
+    }
+    buf.WriteByte('}')
+    return buf.String()
 }
 
 //!-string
+
+func (s *IntSet) Elems() []int {
+    e := make([]int, 0)
+    for i, word := range s.words {
+        for j := 0; j < wordSize; j++ {
+            if word&(1<<uint(j)) != 0 {
+                e = append(e, i*wordSize+j)
+            }
+        }
+    }
+    return e
+}
--- ch6/intset/intset_test.go
+++ ch6/intset/intset_test.go
@@ -4,6 +4,118 @@
 package intset
 
 import "fmt"
+import "testing"
+
+func TestLenZeroInitially(t *testing.T) {
+	s := &IntSet{}
+	if s.Len() != 0 {
+		t.Logf("%d != 0", s.Len())
+		t.Fail()
+	}
+}
+
+func TestLenAfterAddingElements(t *testing.T) {
+	s := &IntSet{}
+	s.Add(0)
+	s.Add(2000)
+	if s.Len() != 2 {
+		t.Logf("%d != 2", s.Len())
+		t.Fail()
+	}
+}
+
+func TestRemove(t *testing.T) {
+	s := &IntSet{}
+	s.Add(0)
+	s.Remove(0)
+	if s.Has(0) {
+		t.Log(s)
+		t.Fail()
+	}
+}
+
+func TestClear(t *testing.T) {
+	s := &IntSet{}
+	s.Add(0)
+	s.Add(1000)
+	s.Clear()
+	if s.Has(0) || s.Has(1000) {
+		t.Log(s)
+		t.Fail()
+	}
+}
+
+func TestCopy(t *testing.T) {
+	orig := &IntSet{}
+	orig.Add(1)
+	copy := orig.Copy()
+	copy.Add(2)
+	if !copy.Has(1) || orig.Has(2) {
+		t.Log(orig, copy)
+		t.Fail()
+	}
+}
+
+func TestAddAll(t *testing.T) {
+    s := &IntSet{}
+    s.AddAll(0, 2, 4)
+    if !s.Has(0) || !s.Has(2) || !s.Has(4) {
+        t.Log(s)
+        t.Fail()
+    }
+}
+
+func TestIntersectWith(t *testing.T) {
+    s1 := &IntSet{}
+    s2 := &IntSet{}
+    s1.AddAll(0,2,4)
+    s2.AddAll(1,2,3)
+    s1.IntersectWith(s2)
+    if !s1.Has(2) || s1.Len() != 1 {
+        t.Log(s1)
+        t.Fail()
+    }
+}
+
+func TestDifferenceWith(t *testing.T) {
+    s := &IntSet{}
+    s.AddAll(0,2,4)
+    u := &IntSet{}
+    u.AddAll(1,2,3)
+    s.DifferenceWith(u)
+    expected := &IntSet{}
+    expected.AddAll(0,4)
+    if s.String() != expected.String() {
+        t.Log(s)
+        t.Fail()
+    }
+}
+
+func TestSymmetricDifferenceWith(t *testing.T) {
+    s := &IntSet{}
+    s.AddAll(0,2,4)
+    u := &IntSet{}
+    u.AddAll(1,2,3)
+    s.SymmetricDifferenceWith(u)
+    expected := &IntSet{}
+    expected.AddAll(0,1,3,4)
+    if s.String() != expected.String() {
+        t.Log(s)
+        t.Fail()
+    }
+}
+
+func TestElem(t *testing.T) {
+    elems := []int{0,2,4,6}
+    s := &IntSet{}
+    s.AddAll(elems...)
+    for i, n := range s.Elems() {
+        if elems[i] != n{
+            t.Log(s.Elems())
+            t.Fail()
+        }
+    }
+}
 
 func Example_one() {
 	//!+main
